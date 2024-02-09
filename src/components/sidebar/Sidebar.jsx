ff0557fcf6883e5aa85a12e55fd72af1b43482f0
shahan210 <mohammedshahan210@gmail.com>
2024-02-09 15:11:01 +0530

    update: sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,9 @@
 import Logo from "../../assets/Logo2.png";
 import { LuLayoutDashboard } from "react-icons/lu";
+import { IoNewspaperOutline } from "react-icons/io5";
+import { BiMessageDetail } from "react-icons/bi";
+import { GrSearch } from "react-icons/gr";
+import { IoSettingsOutline } from "react-icons/io5";
 export default function Sidebar() {
   return (
     <div className="sidebar">
@@ -8,14 +12,30 @@ export default function Sidebar() {
         Stockings
       </div>
       <div className="sidebar-navigation">
-        <div className="sidebar-menu">
-          <LuLayoutDashboard className="menu-icons" />
+        <button className="sidebar-menu active">
+          <LuLayoutDashboard className="menu-icons " />
           Dashboard
-        </div>
-        <div className="sidebar-menu">News</div>
-        <div className="sidebar-menu">Message</div>
-        <div className="sidebar-menu">Analysis</div>
-        <div className="sidebar-menu">settings</div>
+        </button>
+        <br />
+        <button className="sidebar-menu">
+          <IoNewspaperOutline className="menu-icons" />
+          News
+        </button>
+        <br />
+        <button className="sidebar-menu">
+          <BiMessageDetail className="menu-icons" />
+          Message
+        </button>
+        <br />
+        <button className="sidebar-menu ">
+          <GrSearch className="menu-icons" />
+          Analysis
+        </button>
+        <br />
+        <button className="sidebar-menu">
+          <IoSettingsOutline className="menu-icons" />
+          settings
+        </button>
       </div>
     </div>
   );
