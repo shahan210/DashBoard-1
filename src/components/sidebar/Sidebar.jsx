fc70b3d7a03c4aece19d2be5caa7923e72ac908e
shahan210 <mohammedshahan210@gmail.com>
2024-02-12 09:28:47 +0530

    chore: Dashboard layout

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,40 +1,40 @@
 import Logo from "../../assets/Logo2.png";
-import { LuLayoutDashboard } from "react-icons/lu";
-import { IoNewspaperOutline } from "react-icons/io5";
-import { BiMessageDetail } from "react-icons/bi";
-import { GrSearch } from "react-icons/gr";
+import { MdDashboard } from "react-icons/md";
+import { IoNewspaperSharp } from "react-icons/io5";
+import { BiSolidMessageSquareDetail } from "react-icons/bi";
+import { MdBarChart } from "react-icons/md";
 import { IoSettingsOutline } from "react-icons/io5";
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar-header">
         <img src={Logo} alt="logo" className="sidebar-logo" />
-        Stockings
+        Stewardship
       </div>
       <div className="sidebar-navigation">
         <button className="sidebar-menu active">
-          <LuLayoutDashboard className="menu-icons " />
+          <MdDashboard className="menu-icons " />
           Dashboard
         </button>
         <br />
         <button className="sidebar-menu">
-          <IoNewspaperOutline className="menu-icons" />
+          <IoNewspaperSharp className="menu-icons" />
           News
         </button>
         <br />
         <button className="sidebar-menu">
-          <BiMessageDetail className="menu-icons" />
+          <BiSolidMessageSquareDetail className="menu-icons" />
           Message
         </button>
         <br />
         <button className="sidebar-menu ">
-          <GrSearch className="menu-icons" />
-          Analysis
+          <MdBarChart className="menu-icons" />
+          Analytics
         </button>
         <br />
         <button className="sidebar-menu">
           <IoSettingsOutline className="menu-icons" />
-          settings
+          Settings
         </button>
       </div>
     </div>
