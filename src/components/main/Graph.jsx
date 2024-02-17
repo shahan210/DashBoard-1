2a30b63bd65b4e9404e9124f3eeb33b2239abc7b
shahan210 <mohammedshahan210@gmail.com>
2024-02-17 13:37:52 +0530

    update : Dash graph

--- src/components/main/Graph.jsx
+++ src/components/main/Graph.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+import { UserData } from "./Data";
 
 export default function Graph() {
+  const [userData, setUserData] = useState({
+    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5],
+    datasets: [
+      {
+        label: "ersUs Gained",
+        data: [4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 4],
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+      },
+    ],
+    innerWidth: 101,
+  });
   return (
     <div
       className="dash-grid-span-3"
@@ -10,41 +30,8 @@ export default function Graph() {
     >
       <div className="container">
         <div style={{ textAlign: "center" }}>ExpensesDnd</div>
-        <div className="income-list">
-          <div>Maintenance</div>
-          <div>10000</div>
-        </div>
-        <div className="income-list">
-          <div>Maintenance</div>
-          <div>10000</div>
-        </div>
-        <div className="income-list">
-          <div>Maintenance</div>
-          <div>10000</div>
-        </div>
-        <div className="income-list">
-          <div>Maintenance</div>
-          <div>10000</div>
-        </div>
-        <div className="income-list">
-          <div>Maintenance</div>
-          <div>10000</div>
-        </div>
-        <div className="income-list">
-          <div>Maintenance</div>
-          <div>10000</div>
-        </div>
-        <div className="income-list">
-          <div>Maintenance</div>
-          <div>10000</div>
-        </div>{" "}
-        <div className="income-list">
-          <div>Maintenance</div>
-          <div>10000</div>
-        </div>{" "}
-        <div className="income-list">
-          <div>Maintenance</div>
-          <div>10000</div>
+        <div className="dash-graph">
+          <Bar data={userData} width={500} />
         </div>
       </div>
     </div>
