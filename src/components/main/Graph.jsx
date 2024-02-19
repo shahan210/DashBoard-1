55d38d8fe894555771d1262fd0cad684f31561ac
shahan210 <mohammedshahan210@gmail.com>
2024-02-19 17:48:09 +0530

    update :dash graph

--- src/components/main/Graph.jsx
+++ src/components/main/Graph.jsx
@@ -1,26 +1,73 @@
-import React, { useState } from "react";
-import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
-import { UserData } from "./Data";
-
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 export default function Graph() {
-  const [userData, setUserData] = useState({
-    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5],
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: "top",
+      },
+      title: {
+        display: false,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
+  const data = {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
     datasets: [
       {
-        label: "ersUs Gained",
-        data: [4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 4],
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
+        label: "Income",
+        data: [0, 4, 5, 6, 5, 5, 4, 5, 4, 2],
+        borderColor: "#008140",
+        backgroundColor: "#008140",
+      },
+      {
+        label: "Expense",
+        data: [0, 3, 2, 4, 3, 3, 4, 2, 4, 3],
+        borderColor: "#D90001",
+        backgroundColor: "#D90001",
+      },
+      {
+        label: "Savings",
+        data: [0, 1, 3, 4, 2, 2, 3, 3, 2, 4],
+        borderColor: "#111111",
+        backgroundColor: "#111111",
       },
     ],
-    innerWidth: 101,
-  });
+  };
   return (
     <div
       className="dash-grid-span-3"
@@ -28,10 +75,10 @@ export default function Graph() {
         margin: 5,
       }}
     >
-      <div className="container">
+      <div className="container graph-container">
         <div style={{ textAlign: "center" }}>ExpensesDnd</div>
         <div className="dash-graph">
-          <Bar data={userData} width={500} />
+          <Line options={options} data={data} />
         </div>
       </div>
     </div>
